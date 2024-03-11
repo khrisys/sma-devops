2323bacde93e4e5d3d4e4c1ab6fa2bf80aa8be22
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-11 17:17:39 +0100

    composant garage ok

--- application-frontend/src/app/garage/garage.component.ts
+++ application-frontend/src/app/garage/garage.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {CommonModule, NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-garage',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf, HttpClientModule, CommonModule
+  ],
   templateUrl: './garage.component.html',
   styleUrl: './garage.component.css'
 })
-export class GarageComponent {
+export class GarageComponent implements OnInit {
+  garages: any;
 
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get("http://localhost:8888/GARAGE-SERVICE/garages").subscribe({
+      next: data => {
+        this.garages = data;
+      },
+      error: err => {
+        console.log(err)
+      }
+    })
+  }
 }
