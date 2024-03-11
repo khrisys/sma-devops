18558f03e458c7c0fe98225eab26cf73f4f1df01
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-11 21:06:22 +0100

    composant angular pour les clients

--- application-frontend/src/app/client/client.component.ts
+++ application-frontend/src/app/client/client.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {error} from "@angular/compiler-cli/src/transformers/util";
+import {CommonModule, NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf, HttpClientModule, CommonModule
+  ],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
+  clients: any;
 
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get("http://localhost:8888/CLIENT-SERVICE/clients").subscribe({
+      next: data => {
+        this.clients = data;
+      }, error: err => {
+        console.log(err)
+      }
+    })
+  }
 }
