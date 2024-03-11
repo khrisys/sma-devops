364acfa5286fc70f1773a8826deef37ddfc70938
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-11 17:17:24 +0100

    routes des composants

--- application-frontend/src/app/app.routes.ts
+++ application-frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {GarageComponent} from "./garage/garage.component";
+import {ClientComponent} from "./client/client.component";
+import {VehiculeComponent} from "./vehicule/vehicule.component";
+import {DocumentComponent} from "./document/document.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : "garages", component:GarageComponent},
+  {path : "clients", component:ClientComponent},
+  {path: "vehicules", component:VehiculeComponent},
+  {path: "documents", component: DocumentComponent}
+];
