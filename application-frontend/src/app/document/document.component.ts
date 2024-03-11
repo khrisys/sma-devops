afa8ae5b90df3097c615a1641c2c043f5bc4873c
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-11 21:50:21 +0100

    composant angular pour les documents

--- application-frontend/src/app/document/document.component.ts
+++ application-frontend/src/app/document/document.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-document',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, NgForOf, NgIf],
   templateUrl: './document.component.html',
   styleUrl: './document.component.css'
 })
-export class DocumentComponent {
+export class DocumentComponent implements OnInit{
+documents:any;
+  constructor(private http:HttpClient) {
+  }
 
+  ngOnInit() {
+    this.http.get("http://localhost:8888/DOCUMENT-SERVICE/documents").subscribe({
+      next: data => {
+        this.documents = data;
+      }, error: err => {
+        console.log(err)
+      }
+    })
+  }
 }
