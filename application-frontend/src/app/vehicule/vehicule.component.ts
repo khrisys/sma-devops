682cafa50a1861669155ecb4092767831ee578fc
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-11 21:50:32 +0100

    composant angular pour les vehicules

--- application-frontend/src/app/vehicule/vehicule.component.ts
+++ application-frontend/src/app/vehicule/vehicule.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-vehicule',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, NgForOf, NgIf],
   templateUrl: './vehicule.component.html',
   styleUrl: './vehicule.component.css'
 })
-export class VehiculeComponent {
+export class VehiculeComponent implements OnInit{
+vehicules: any;
+  constructor(private http: HttpClient) {
+  }
 
+  ngOnInit() {
+    this.http.get("http://localhost:8888/VEHICULE-SERVICE/vehicules").subscribe({
+      next: data => {
+        this.vehicules = data;
+      }, error: err => {
+        console.log(err)
+      }
+    })
+  }
 }
