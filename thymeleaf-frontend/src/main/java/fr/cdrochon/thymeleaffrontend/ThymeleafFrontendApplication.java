bc5d876ef0cd480c93b51879142e25e84f836f99
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-13 10:39:35 +0100

    jeu de données pour les clients

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/ThymeleafFrontendApplication.java
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/ThymeleafFrontendApplication.java
@@ -1,7 +1,9 @@
 package fr.cdrochon.thymeleaffrontend;
 
-import fr.cdrochon.thymeleaffrontend.entity.AdresseGarage;
+import fr.cdrochon.thymeleaffrontend.entity.Adresse;
+import fr.cdrochon.thymeleaffrontend.entity.Client;
 import fr.cdrochon.thymeleaffrontend.entity.Garage;
+import fr.cdrochon.thymeleaffrontend.repository.ClientRepository;
 import fr.cdrochon.thymeleaffrontend.repository.GarageRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,20 +20,20 @@ public class ThymeleafFrontendApplication {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(GarageRepository garageRepository) {
+    CommandLineRunner commandLineRunner(GarageRepository garageRepository, ClientRepository clientRepository) {
         return args -> {
 
 
-            AdresseGarage adresse1 = AdresseGarage.builder()
+            Adresse adresse1 = Adresse.builder()
                     .rue("rue du truc")
                     .numeroDeRue("17")
                     .cp("33300")
                     .ville("St Jean")
                     .build();
-            AdresseGarage adresse2 = AdresseGarage.builder().rue("avenue du jo").cp("59140").numeroDeRue("51").ville(
+            Adresse adresse2 = Adresse.builder().rue("avenue du jo").cp("59140").numeroDeRue("51").ville(
                     "Cloerwak").build();
 
-            List<Garage> customerList = List.of(
+            List<Garage> garageList = List.of(
 
 
                     Garage.builder()
@@ -46,7 +48,34 @@ public class ThymeleafFrontendApplication {
                             .build()
 
             );
-            garageRepository.saveAll(customerList);
+            garageRepository.saveAll(garageList);
+
+            List<Client> clientList = List.of(
+
+
+                    Client.builder()
+                            .nomClient("Dubourg")
+                            .prenomClient("Jean")
+                            .mailClient("jean.dubourg@gmail.com")
+                            .telClient("0123456789")
+                            .adresseClient(adresse1)
+                            .build(),
+                    Client.builder()
+                            .nomClient("Berga")
+                            .prenomClient("Christine")
+                            .mailClient("chirstine.berga@tut.io")
+                            .telClient("9876541320")
+                            .adresseClient(adresse2)
+                            .build(),
+                    Client.builder()
+                            .nomClient("Poloua")
+                            .prenomClient("Gerard")
+                            .mailClient("gegelembrouille@tut.io")
+                            .telClient("8888888888")
+                            .adresseClient(adresse2)
+                            .build()
+            );
+            clientRepository.saveAll(clientList);
         };
     }
 
