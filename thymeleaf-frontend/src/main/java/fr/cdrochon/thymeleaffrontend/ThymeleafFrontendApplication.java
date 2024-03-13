cdc6c0fc1e6ac44d2ddc2910c235ac94453505dd
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-13 08:29:26 +0100

    ajout du module thymeleaf

---
+++ thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/ThymeleafFrontendApplication.java
@@ -0,0 +1,13 @@
+package fr.cdrochon.thymeleaffrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafFrontendApplication.class, args);
+    }
+
+}
---
+++ thymeleaf-frontend/src/test/java/fr/cdrochon/thymeleaffrontend/ThymeleafFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cdrochon.thymeleaffrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
