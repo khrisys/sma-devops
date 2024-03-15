4e63792b0c43d57c84fb2493997461055c374c02
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-15 11:51:23 +0100

    creation des component du projet

--- angular-module-frontend/src/app/app.module.ts
+++ angular-module-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GarageComponent } from './garage/garage.component';
+import { ClientComponent } from './client/client.component';
+import { VehiculeComponent } from './vehicule/vehicule.component';
+import { DocumentComponent } from './document/document.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GarageComponent,
+    ClientComponent,
+    VehiculeComponent,
+    DocumentComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-module-frontend/src/app/vehicule/vehicule.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicule',
+  templateUrl: './vehicule.component.html',
+  styleUrl: './vehicule.component.css'
+})
+export class VehiculeComponent {
+
+}
