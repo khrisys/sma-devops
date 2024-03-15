26b8031e6f0b02fb7d49afbbe1f166bc56f809cc
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-15 20:03:31 +0100

    recup données client depuis ms

--- angular-module-frontend/src/app/client/client.component.ts
+++ angular-module-frontend/src/app/client/client.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit{
 
+  clients: any;
+  constructor(private http: HttpClient) {
+  }
+
+  /**
+   * Recupere les données  à l'initialisation du component
+   */
+  ngOnInit() {
+    this.http.get("http://localhost:8082/clients").subscribe({
+      next: value => {
+        this.clients = value;
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
 }
