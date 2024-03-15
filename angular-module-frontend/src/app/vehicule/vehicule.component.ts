c5c562cdd51506dc12ef22b823723e2450634c17
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-15 20:37:56 +0100

    recup et affichage des données du ms vehicule

--- angular-module-frontend/src/app/vehicule/vehicule.component.ts
+++ angular-module-frontend/src/app/vehicule/vehicule.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-vehicule',
   templateUrl: './vehicule.component.html',
   styleUrl: './vehicule.component.css'
 })
-export class VehiculeComponent {
+export class VehiculeComponent implements OnInit {
+  vehicules: any;
 
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get("http://localhost:8083/vehicules").subscribe({
+      next: value => {
+        this.vehicules = value;
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
 }
