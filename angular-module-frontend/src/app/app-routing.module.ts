fca1d4d1e5ecc30fce9dead3dd4ba6f080f3b2d4
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-15 12:16:38 +0100

    declaration des routes des components

--- angular-module-frontend/src/app/app-routing.module.ts
+++ angular-module-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {GarageComponent} from "./garage/garage.component";
+import {ClientComponent} from "./client/client.component";
+import {VehiculeComponent} from "./vehicule/vehicule.component";
+import {DocumentComponent} from "./document/document.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "garages", component: GarageComponent},
+  {path: "clients", component: ClientComponent},
+  {path: "vehicules", component: VehiculeComponent},
+  {path: "documents", component: DocumentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
