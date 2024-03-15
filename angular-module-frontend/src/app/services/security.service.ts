c26cc11dbed5b3685e33b83c24a79c12689274f8
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-15 20:04:03 +0100

    service d'authentification et gestion du login et logout

---
+++ angular-module-frontend/src/app/services/security.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from "@angular/core";
+import {KeycloakProfile} from "keycloak-js";
+import {KeycloakEventType, KeycloakService} from "keycloak-angular";
+
+/**
+ * profile contient toutes infos de l'user
+ */
+@Injectable({providedIn : "root"})
+export class SecurityService {
+  public profile?: KeycloakProfile;
+
+  constructor(public kcService: KeycloakService) {
+    this.init();
+  }
+
+  init() {
+    console.log("Init ....")
+    this.kcService.keycloakEvents$.subscribe({
+      next: (e) => {
+        console.log(e);
+        if (e.type == KeycloakEventType.OnAuthSuccess) {
+          console.log("OnAuthSuccess")
+          this.kcService.loadUserProfile().then(profile => {
+            this.profile = profile;
+          });
+        }
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+
+  /**
+   * Retourne vrai ou faux lorsqu'on demande si un usre possede un role particulier
+   * @param roles
+   */
+  public hasRoleIn(roles: string[]): boolean {
+    let userRoles = this.kcService.getUserRoles();
+    for (let role of roles) {
+      if (userRoles.includes(role)) return true;
+    }
+    return false;
+  }
+}
