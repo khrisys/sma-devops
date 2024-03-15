c7533cd2cf4ea0c0754d3abdb56d7961f3790bdf
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-15 16:11:06 +0100

    requete depuis angular pour recup les données garage

--- angular-module-frontend/src/app/garage/garage.component.ts
+++ angular-module-frontend/src/app/garage/garage.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
   selector: 'app-garage',
   templateUrl: './garage.component.html',
   styleUrl: './garage.component.css'
 })
-export class GarageComponent {
+export class GarageComponent implements OnInit {
+  garages: any;
+  constructor(private http: HttpClient) {
+  }
 
+  /**
+   * Recupere les données  à l'initialisation du component
+   */
+  ngOnInit() {
+    this.http.get("http://localhost:8081").subscribe({
+      next: value => {
+        this.garages = value;
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
 }
