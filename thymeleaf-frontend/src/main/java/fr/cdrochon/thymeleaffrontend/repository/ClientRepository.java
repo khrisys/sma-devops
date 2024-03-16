5f590540291f4a22e6b14b8ef6c55e0f3814d0eb
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-16 12:33:26 +0100

    suppression des repo dans thymeleaf qui ne servent plus vu que je recupere les données des ms via des requetes http

--- thymeleaf-frontend/src/main/java/fr/cdrochon/thymeleaffrontend/repository/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.cdrochon.thymeleaffrontend.repository;
-
-
-import fr.cdrochon.thymeleaffrontend.entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
