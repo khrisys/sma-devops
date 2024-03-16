f459e91a960b5b76d11b3eee58e495e50117ee28
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-16 10:02:45 +0100

    recupe documents depuis angular vers ms document

--- angular-module-frontend/src/app/document/document.component.ts
+++ angular-module-frontend/src/app/document/document.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-document',
   templateUrl: './document.component.html',
   styleUrl: './document.component.css'
 })
-export class DocumentComponent {
+export class DocumentComponent implements OnInit {
+  documents: any;
 
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get("http://localhost:8084/documents").subscribe({
+      next: value => {
+        this.documents = value;
+      }, error: err => {
+        console.log(err);
+      }
+    })
+  }
 }
