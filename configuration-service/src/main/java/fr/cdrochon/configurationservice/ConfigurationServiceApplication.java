d7663fd1c918e329861dedf58c0eb2512045e445
christiandrochon <khrisys@protonmail.com>
2024-04-13 19:19:44 +0200

    Activation du service de configuration configuration-service que j'avais supprimé en testant les ms seuls

--- configuration-service/src/main/java/fr/cdrochon/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/fr/cdrochon/configurationservice/ConfigurationServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-//@EnableConfigServer
+@EnableConfigServer
 public class ConfigurationServiceApplication {
 
     public static void main(String[] args) {
